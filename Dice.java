be22226c9c4d1c79f877dd689d24fae91c1d0a39
SAC-CS112-Lopez-Edson <edson@ipcmobile.com>
2017-04-06 08:57:52 -0700

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,30 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rollEm;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-/**
- *
- * @author edsonlopez
- */
-public class Dice {
-    
-    public static void main(String args[])
-	{
-            boolean replay = true;
-            int count1 = 1;
-            int count2 = 1;
-            int count3 = 1;
-            double winRate = 0;
-            boolean play = true;
-            String again;
-            
-	  /*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
